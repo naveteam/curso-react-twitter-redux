b13c1b8c1ee7c8189f87b9383eb9182a4ee6a4b3
João Pedro Bretanha <joaopedro@nave.rs>
2017-11-24 21:07:03 -0200

    changed API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const __API__ = 'http://localhost:8080';
-// const __API__ = 'https://api-nave-twitter.herokuapp.com';
+// const __API__ = 'http://localhost:8080';
+const __API__ = 'https://api-nave-twitter.herokuapp.com';
 export default axios.create({
     baseURL: __API__,
 })
